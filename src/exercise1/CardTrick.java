3fb5431d2ae543bbe0cb15c798407ff26b77d859
Pankaj Pankaj <panpanka@sheridancollege.ca>
2023-02-02 23:09:24 -0500

    I'm Done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -69,6 +69,7 @@ public class CardTrick {
      * replace this information with your own.
      * @author Pankaj Pankaj Feb 2023
      */
+    // I'm Done
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
