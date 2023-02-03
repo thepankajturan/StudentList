12652d6319e33a2290771bbafb0c31f4cd2c3b41
Pankaj Pankaj <panpanka@sheridancollege.ca>
2023-02-02 22:43:28 -0500

    completed the "CardTrick" class

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,7 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -11,16 +13,18 @@ package exercise1;
 public class CardTrick {
     
     public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
         
         Card[] hand = new Card[7];
-
+        
+        // Fill the hand with random cards
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue(random.nextInt(13) + 1);
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
+            hand[i] = card;
         }
 
         // insert code to ask the user for Card value and suit, create their card
@@ -31,8 +35,33 @@ public class CardTrick {
         // 
         // Then loop through the cards in the array to see if there's a match.
         
-        // If the guess is successful, print System.out.println("Congratulations, you guessed right!");.
+         // Ask the user for a card
+        System.out.println("Pick a card: enter the value (1-13) and the suit (1-4)");
+        System.out.print("Value: ");
+        int value = sc.nextInt();
+        System.out.print("Suit: ");
+        int suit = sc.nextInt();
+        Card userCard = new Card();
+        userCard.setValue(value);
+        userCard.setSuit(Card.SUITS[suit - 1]);
         
+        // If the guess is successful, print System.out.println("Congratulations, you guessed right!");.
+         // Search for the user's card in the hand
+        boolean found = false;
+        for (int i = 0; i < hand.length; i++) {
+            if (hand[i].getValue() == userCard.getValue() && hand[i].getSuit().equals(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+         // Print the result
+        if (found) {
+            System.out.println("Congratulations, you guessed right!");
+        } else {
+            System.out.println("Sorry, that card is not in the hand.");
+        }
+        
+        sc.close();
     }
 
     /**
